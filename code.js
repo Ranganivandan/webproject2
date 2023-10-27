259b7a95e42905a1b40a5ae1ff3809f01c10621f
Ranganivandan <147639923+Ranganivandan@users.noreply.github.com>
2023-10-27 20:58:49 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,12 @@
+
+setInterval(updatetime,1000);
+function updatetime() {
+    time.innerHTML= new Date();
+}
+
+
+
+
+  
+
+    signup.addEventListener("click", function(){ alert("Hello World!"); });
